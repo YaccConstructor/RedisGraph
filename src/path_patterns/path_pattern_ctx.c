6aa61262d3998bdae4c8ce7f5db4d439421bbde0
simletonsl <simpletondl@yandex.ru>
2020-04-01 19:18:38 +0300

    add checking NULL pointer while free path pattern ctx

--- src/path_patterns/path_pattern_ctx.c
+++ src/path_patterns/path_pattern_ctx.c
@@ -75,9 +75,11 @@ PathPatternCtx *BuildPathPatternCtx(AST *ast, size_t required_dim) {
 }
 
 void PathPatternCtx_Free(PathPatternCtx *pathPatternCtx) {
-    for (int i = 0; i < array_len(pathPatternCtx->patterns); ++i) {
-        PathPattern_Free(pathPatternCtx->patterns[i]);
+    if (pathPatternCtx != NULL) {
+        for (int i = 0; i < array_len(pathPatternCtx->patterns); ++i) {
+            PathPattern_Free(pathPatternCtx->patterns[i]);
+        }
+        array_free(pathPatternCtx->patterns);
+        rm_free(pathPatternCtx);
     }
-    array_free(pathPatternCtx->patterns);
-    rm_free(pathPatternCtx);
 }
