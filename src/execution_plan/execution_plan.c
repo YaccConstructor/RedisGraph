709e9b0a1423f23637936319021776a50bc71f97
simletonsl <simpletondl@yandex.ru>
2020-05-05 22:16:45 +0300

    free path pattern ctx

--- src/execution_plan/execution_plan.c
+++ src/execution_plan/execution_plan.c
@@ -1060,6 +1060,7 @@ static void _ExecutionPlan_FreeSubPlan(ExecutionPlan *plan) {
 	}
 
 	QueryGraph_Free(plan->query_graph);
+	PathPatternCtx_Free(plan->path_pattern_ctx);
     if(plan->record_map) raxFree(plan->record_map);
 	if(plan->record_pool) ObjectPool_Free(plan->record_pool);
 	if(plan->ast_segment) AST_Free(plan->ast_segment);
@@ -1089,6 +1090,7 @@ void ExecutionPlan_Free(ExecutionPlan *plan) {
 	}
 
 	QueryGraph_Free(plan->query_graph);
+	PathPatternCtx_Free(plan->path_pattern_ctx);
 	if(plan->record_map) raxFree(plan->record_map);
 	if(plan->record_pool) ObjectPool_Free(plan->record_pool);
 	if(plan->ast_segment) AST_Free(plan->ast_segment);
