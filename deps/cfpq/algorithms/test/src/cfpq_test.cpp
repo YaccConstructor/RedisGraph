6d55594326d9f2b4868540498837186f645df564
akhoroshev <arthoroshev@gmail.com>
2020-05-20 13:41:53 +0300

    [CUDA] uncomment tests

--- deps/cfpq/algorithms/test/src/cfpq_test.cpp
+++ deps/cfpq/algorithms/test/src/cfpq_test.cpp
@@ -115,117 +115,117 @@ TEST_P(CFPQTestNsparse, SmallGraph) {
                       {"b", 2},
                   });
 }
-//
-//TEST_P(CFPQTestNsparse, RdfPathwaysG1) {
-//  auto solver = GetParam();
-//  auto response = run(
-//      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/pathways.txt",
-//      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
-//  check(response, {});
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfPathwaysG2) {
-//  auto solver = GetParam();
-//  auto response = run(
-//      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/pathways.txt",
-//      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
-//  check(response, {});
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfGoHierarchyG1) {
-//  auto solver = GetParam();
-//  auto response = run("resources/rdf/matrices/go-hierarchy.txt",
-//                      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
-//  check(response, {
-//                      {"s", 588976},
-//                      {"s1", 490109},
-//                      {"s2", 490109},
-//                      {"s3", 0},
-//                      {"s4", 0},
-//                      {"s5", 324016},
-//                      {"s6", 0},
-//                  });
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfGoHierarchyG2) {
-//  auto solver = GetParam();
-//  auto response = run("resources/rdf/matrices/go-hierarchy.txt",
-//                      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
-//  check(response, {});
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfEnzymeG1) {
-//  auto solver = GetParam();
-//  auto response = run(
-//      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/enzyme.txt",
-//      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
-//  check(response, {});
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfEnzymeG2) {
-//  auto solver = GetParam();
-//  auto response = run(
-//      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/enzyme.txt",
-//      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
-//  check(response, {});
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfEclassG1) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/"
-//          "eclass_514en.txt",
-//          "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
-//  check(response, {});
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfEclassG2) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/"
-//          "eclass_514en.txt",
-//          "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
-//  check(response, {});
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfGoGPPerf1) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("resources/rdf/matrices/go.txt", "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
-//  check(response, {
-//                      {"s", 304070},
-//                      {"s1", 90512},
-//                      {"s2", 90512},
-//                      {"s3", 58483},
-//                      {"s4", 58483},
-//                      {"s5", 278610},
-//                      {"s6", 39642},
-//                  });
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfGoGPPerf2) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("resources/rdf/matrices/go.txt", "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
-//  check(response, {
-//                      {"b", 334850},
-//                      {"s1", 90512},
-//                      {"s2", 90512},
-//                      {"s3", 327628},
-//                  });
-//}
-//
-//TEST_P(CFPQTestNsparse, RdfGeospecies) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("resources/rdf/matrices/geospeices.txt", "resources/rdf/grammars/geo.cnf", solver);
-//  check(response, {
-//                      {"s", 226669749},
-//                      {"bt", 20867},
-//                      {"s1", 21361542},
-//                      {"btr", 20867},
-//                  });
-//}
+
+TEST_P(CFPQTestNsparse, RdfPathwaysG1) {
+  auto solver = GetParam();
+  auto response = run(
+      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/pathways.txt",
+      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
+  check(response, {});
+}
+
+TEST_P(CFPQTestNsparse, RdfPathwaysG2) {
+  auto solver = GetParam();
+  auto response = run(
+      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/pathways.txt",
+      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
+  check(response, {});
+}
+
+TEST_P(CFPQTestNsparse, RdfGoHierarchyG1) {
+  auto solver = GetParam();
+  auto response = run("resources/rdf/matrices/go-hierarchy.txt",
+                      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
+  check(response, {
+                      {"s", 588976},
+                      {"s1", 490109},
+                      {"s2", 490109},
+                      {"s3", 0},
+                      {"s4", 0},
+                      {"s5", 324016},
+                      {"s6", 0},
+                  });
+}
+
+TEST_P(CFPQTestNsparse, RdfGoHierarchyG2) {
+  auto solver = GetParam();
+  auto response = run("resources/rdf/matrices/go-hierarchy.txt",
+                      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
+  check(response, {});
+}
+
+TEST_P(CFPQTestNsparse, RdfEnzymeG1) {
+  auto solver = GetParam();
+  auto response = run(
+      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/enzyme.txt",
+      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
+  check(response, {});
+}
+
+TEST_P(CFPQTestNsparse, RdfEnzymeG2) {
+  auto solver = GetParam();
+  auto response = run(
+      "/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/enzyme.txt",
+      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
+  check(response, {});
+}
+
+TEST_P(CFPQTestNsparse, RdfEclassG1) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/"
+          "eclass_514en.txt",
+          "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
+  check(response, {});
+}
+
+TEST_P(CFPQTestNsparse, RdfEclassG2) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/CFPQ_Data/data/graphs/RDF/Matrices/"
+          "eclass_514en.txt",
+          "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
+  check(response, {});
+}
+
+TEST_P(CFPQTestNsparse, RdfGoGPPerf1) {
+  auto solver = GetParam();
+  auto response =
+      run("resources/rdf/matrices/go.txt", "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
+  check(response, {
+                      {"s", 304070},
+                      {"s1", 90512},
+                      {"s2", 90512},
+                      {"s3", 58483},
+                      {"s4", 58483},
+                      {"s5", 278610},
+                      {"s6", 39642},
+                  });
+}
+
+TEST_P(CFPQTestNsparse, RdfGoGPPerf2) {
+  auto solver = GetParam();
+  auto response =
+      run("resources/rdf/matrices/go.txt", "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
+  check(response, {
+                      {"b", 334850},
+                      {"s1", 90512},
+                      {"s2", 90512},
+                      {"s3", 327628},
+                  });
+}
+
+TEST_P(CFPQTestNsparse, RdfGeospecies) {
+  auto solver = GetParam();
+  auto response =
+      run("resources/rdf/matrices/geospeices.txt", "resources/rdf/grammars/geo.cnf", solver);
+  check(response, {
+                      {"s", 226669749},
+                      {"bt", 20867},
+                      {"s1", 21361542},
+                      {"btr", 20867},
+                  });
+}
 
 TEST_P(CFPQTestNsparse, RdfTaxGPPerf1) {
   auto solver = GetParam();
@@ -241,145 +241,145 @@ TEST_P(CFPQTestNsparse, RdfTaxGPPerf1) {
                       {"s6", 0},
                   });
 }
-//
-//TEST_P(CFPQTestNsparse, RdfTaxGPPerf2) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/mnt/data/tmp/taxonomy.txt", "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
-//  check(response, {
-//                      {"b", 36722046},
-//                      {"s1", 35583954},
-//                      {"s2", 35583954},
-//                      {"s3", 33697004},
-//                  });
-//}
-//
-// TEST_P(CFPQTestNsparse, RdfTaxHieGPPerf1) {
-//  auto solver = GetParam();
-//  auto response = run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/taxonomy-hie.txt",
-//                      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
-//  check(response, {
-//                      {"s", 6977582},
-//                      {"s1", 35583954},
-//                      {"s2", 35583954},
-//                      {"s3", 0},
-//                      {"s4", 0},
-//                      {"s5", 4531241},
-//                      {"s6", 0},
-//                  });
-//}
-//
-// TEST_P(CFPQTestNsparse, RdfTaxHieGPPerf2) {
-//  auto solver = GetParam();
-//  auto response = run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/taxonomy-hie.txt",
-//                      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
-//  check(response, {
-//                      {"b", 36722046},
-//                      {"s1", 35583954},
-//                      {"s2", 35583954},
-//                      {"s3", 33697004},
-//                  });
-//}
 
-//// FreeScale
-//// 10k
-// TEST_P(CFPQTestNsparse, FreeScale10000_3) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_10000_3.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-// TEST_P(CFPQTestNsparse, FreeScale10000_5) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_10000_5.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-// TEST_P(CFPQTestNsparse, FreeScale10000_10) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_10000_10.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
+TEST_P(CFPQTestNsparse, RdfTaxGPPerf2) {
+  auto solver = GetParam();
+  auto response =
+      run("/mnt/data/tmp/taxonomy.txt", "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
+  check(response, {
+                      {"b", 36722046},
+                      {"s1", 35583954},
+                      {"s2", 35583954},
+                      {"s3", 33697004},
+                  });
+}
 
-//// 25k
-// TEST_P(CFPQTestNsparse, FreeScale25000_3) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_25000_3.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-// TEST_P(CFPQTestNsparse, FreeScale25000_5) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_25000_5.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-// TEST_P(CFPQTestNsparse, FreeScale25000_10) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_25000_10.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-//
-//// 50k
-// TEST_P(CFPQTestNsparse, FreeScale50000_3) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_50000_3.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-// TEST_P(CFPQTestNsparse, FreeScale50000_5) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_50000_5.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-// TEST_P(CFPQTestNsparse, FreeScale50000_10) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_50000_10.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-//// 100k
-// TEST_P(CFPQTestNsparse, FreeScale100000_3) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_100000_3.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-// TEST_P(CFPQTestNsparse, FreeScale100000_5) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_100000_5.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
-//
-//
-// TEST_P(CFPQTestNsparse, FreeScale100000_10) {
-//  auto solver = GetParam();
-//  auto response =
-//      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_100000_10.txt",
-//          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
-//  check(response, {});
-//}
+ TEST_P(CFPQTestNsparse, RdfTaxHieGPPerf1) {
+  auto solver = GetParam();
+  auto response = run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/taxonomy-hie.txt",
+                      "resources/rdf/grammars/GPPerf1_cnf.txt", solver);
+  check(response, {
+                      {"s", 6977582},
+                      {"s1", 35583954},
+                      {"s2", 35583954},
+                      {"s3", 0},
+                      {"s4", 0},
+                      {"s5", 4531241},
+                      {"s6", 0},
+                  });
+}
+
+ TEST_P(CFPQTestNsparse, RdfTaxHieGPPerf2) {
+  auto solver = GetParam();
+  auto response = run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/taxonomy-hie.txt",
+                      "resources/rdf/grammars/GPPerf2_cnf.txt", solver);
+  check(response, {
+                      {"b", 36722046},
+                      {"s1", 35583954},
+                      {"s2", 35583954},
+                      {"s3", 33697004},
+                  });
+}
+
+// FreeScale
+// 10k
+ TEST_P(CFPQTestNsparse, FreeScale10000_3) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_10000_3.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+ TEST_P(CFPQTestNsparse, FreeScale10000_5) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_10000_5.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+ TEST_P(CFPQTestNsparse, FreeScale10000_10) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_10000_10.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+// 25k
+ TEST_P(CFPQTestNsparse, FreeScale25000_3) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_25000_3.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+ TEST_P(CFPQTestNsparse, FreeScale25000_5) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_25000_5.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+ TEST_P(CFPQTestNsparse, FreeScale25000_10) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_25000_10.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+
+// 50k
+ TEST_P(CFPQTestNsparse, FreeScale50000_3) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_50000_3.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+ TEST_P(CFPQTestNsparse, FreeScale50000_5) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_50000_5.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+ TEST_P(CFPQTestNsparse, FreeScale50000_10) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_50000_10.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+// 100k
+ TEST_P(CFPQTestNsparse, FreeScale100000_3) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_100000_3.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+ TEST_P(CFPQTestNsparse, FreeScale100000_5) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_100000_5.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
+
+
+ TEST_P(CFPQTestNsparse, FreeScale100000_10) {
+  auto solver = GetParam();
+  auto response =
+      run("/home/jblab/CFPQ-with-RedisGraph/akhoroshev/Matrices/free_scale_graph_100000_10.txt",
+          "resources/freescale/grammars/an_bm_cm_dn.txt", solver);
+  check(response, {});
+}
