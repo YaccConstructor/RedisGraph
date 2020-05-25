51ed189de95d94bc99fbaae5b7794ff715149234
Ilya Epelbaum <33521263+IlyaEp@users.noreply.github.com>
2020-05-25 17:32:47 +0300

    Add accelerated version cpu2

--- src/cfpq_algorithms/cfpq_cpu_2.c
+++ src/cfpq_algorithms/cfpq_cpu_2.c
@@ -4,79 +4,84 @@
 #include "../util/mem_prof/mem_prof.h"
 #include "../util/simple_timer.h"
 
+GrB_Index I[30000000];
+GrB_Index J[30000000];
+
 int CFPQ_cpu2(RedisModuleCtx *ctx, GraphContext *gc, bool_automat *grammar, CfpqResponse *response)
 {
+
     MemInfo mem_start;
     mem_usage_tick(&mem_start);
 
     // create automat matrix
-
     GrB_init(GrB_BLOCKING);
-    
+
     int count_automat = grammar->count_matrices;
     uint64_t sizeAutomat = grammar->size_matrices;
-    GrB_Matrix Automat[count_automat];
-    //GrB_Info info = GrB_Matrix_new(&Automat, GrB_INT64, sizeAutomat, sizeAutomat);
+    GrB_Matrix Automat[10];
     
     for (int i = 0; i < count_automat; i++)
     {
-		GrB_Info info = GrB_Matrix_new(&Automat[i], GrB_BOOL, sizeAutomat, sizeAutomat);
-		if (info != GrB_SUCCESS)
+	GrB_Info info = GrB_Matrix_new(&Automat[i], GrB_BOOL, sizeAutomat, sizeAutomat);
+	GxB_set(Automat[i], GxB_HYPER, GxB_HYPER_DEFAULT);
+	if (info != GrB_SUCCESS)
             RedisModule_ReplyWithError(ctx, "failed to construct the matrix\n");
     }
 
     for (int i = 0; i < count_automat; i++)
-		GrB_Matrix_dup(&Automat[i], grammar->matrices[i]);
+	GrB_Matrix_dup(&Automat[i], grammar->matrices[i]);
 
 
     // create states matrix
     int count_S_term = grammar->count_S_term;
-    GrB_Matrix States[count_S_term];
+    GrB_Matrix States[10];
     for (int i = 0; i < count_S_term; i++)
     {
-		GrB_Info info = GrB_Matrix_new(&States[i], GrB_BOOL, sizeAutomat, sizeAutomat);
-		if (info != GrB_SUCCESS)
+	GrB_Info info = GrB_Matrix_new(&States[i], GrB_BOOL, sizeAutomat, sizeAutomat);
+	GxB_set(States[i], GxB_HYPER, GxB_HYPER_DEFAULT);
+	if (info != GrB_SUCCESS)
             RedisModule_ReplyWithError(ctx, "failed to construct the matrix\n");
     }
 
     for (int i = 0; i < count_S_term; i++)
-		GrB_Matrix_dup(&States[i], grammar->matrices[count_automat + i]);
+	GrB_Matrix_dup(&States[i], grammar->matrices[count_automat + i]);
 
     // create graph matrix
-
-    uint64_t count_matrices = GraphContext_SchemaCount(gc, SCHEMA_EDGE) / 2;
+    uint64_t count_matrices = GraphContext_SchemaCount(gc, SCHEMA_EDGE)/2;
     uint64_t sizeGraph = Graph_RequiredMatrixDim(gc->g);
-    GrB_Matrix matrices[count_matrices + count_S_term];
+    GrB_Matrix matrices[10];
 
 	for (uint64_t i = 0; i < count_matrices + count_S_term; i++)
 	{
 		GrB_Info info = GrB_Matrix_new(&matrices[i], GrB_BOOL, sizeGraph, sizeGraph);
+		GxB_set(matrices[i], GxB_HYPER, GxB_HYPER_DEFAULT);
 		if (info != GrB_SUCCESS)
-        	RedisModule_ReplyWithError(ctx, "failed to construct the matrix\n");
+        	    RedisModule_ReplyWithError(ctx, "failed to construct the matrix\n");
 	}
 
-	for (uint64_t i = 0; i < 2 * count_matrices; i += 2)
+	for (uint64_t i = 0; i < 2*count_matrices; i+=2)
 	{
 		char *label = gc->relation_schemas[i]->name;
 		int64_t label_id = map_get_second_by_first(&grammar->indices, label);
 		uint64_t index = 0;
+
+		   //assert(label_id!=INT_MIN);
+		//printf("%s \n", label);
+		if (label_id == INT_MIN)
+		    continue;
+		 printf("%s \n", label);
 		while (grammar->id[index] != label_id && index < count_matrices)
 		{
 			index++;
 		}
 
-		GrB_Matrix_dup(&matrices[index], gc->g->relations[i]);
+		//GrB_Matrix_dup(&matrices[index], gc->g->relations[i]);
+		GrB_eWiseAdd_Matrix_BinaryOp(matrices[index], NULL, NULL, GrB_LOR, matrices[index], gc->g->relations[i], NULL);
 		GrB_eWiseAdd_Matrix_BinaryOp(matrices[index], NULL, NULL, GrB_LOR, matrices[index], gc->g->relations[i + 1], NULL);
 	}
 
+//count_matrices = 2;
 
-
-    GxB_print(matrices[0], 3);
-    GxB_print(matrices[1], 3);
-    GxB_print(matrices[2], 3);
-    printf("%ld", count_matrices);
-
-    
 
     // create bool monoid for bool semiring
     GrB_Monoid monoid;
@@ -93,15 +98,23 @@ int CFPQ_cpu2(RedisModuleCtx *ctx, GraphContext *gc, bool_automat *grammar, Cfpq
     GrB_Matrix Kproduct;
     uint64_t sizeKproduct = sizeGraph * sizeAutomat;
     GrB_Matrix_new(&Kproduct, GrB_BOOL, sizeKproduct, sizeKproduct);
+    GxB_set(Kproduct, GxB_HYPER, GxB_HYPER_DEFAULT);
     GrB_Matrix degreeKproduct;
     GrB_Matrix_new(&degreeKproduct, GrB_BOOL, sizeKproduct, sizeKproduct);
+    GxB_set(degreeKproduct, GxB_HYPER, GxB_HYPER_DEFAULT);
     GrB_Matrix block;
     GrB_Matrix_new(&block, GrB_BOOL, sizeGraph, sizeGraph);
+    GxB_set(block, GxB_HYPER, GxB_HYPER_DEFAULT);
 
-	GrB_Info info;
+    GrB_Info info;
 	
     GrB_Matrix partKproduct;
-	GrB_Matrix_new(&partKproduct, GrB_BOOL, sizeKproduct, sizeKproduct);
+    GrB_Matrix_new(&partKproduct, GrB_BOOL, sizeKproduct, sizeKproduct);
+    GxB_set(partKproduct, GxB_HYPER, GxB_HYPER_DEFAULT);
+
+    GrB_Matrix prevG;
+    GrB_Matrix_new(&prevG, GrB_BOOL, sizeGraph, sizeGraph);
+    GxB_set(prevG, GxB_HYPER, GxB_HYPER_DEFAULT);
 
     // algorithm
     bool matrices_is_changed = true;
@@ -111,18 +124,17 @@ int CFPQ_cpu2(RedisModuleCtx *ctx, GraphContext *gc, bool_automat *grammar, Cfpq
         matrices_is_changed = false;
         response->iteration_count++;
 		
-		for (uint64_t i = 0; i < count_matrices; i++)
-    	{
-        	GxB_kron(partKproduct, NULL, NULL, GrB_LAND, Automat[i], matrices[i], NULL);
-        	GrB_eWiseAdd_Matrix_BinaryOp(Kproduct, NULL, NULL, GrB_LOR, Kproduct, partKproduct, NULL);
-        	GrB_Matrix_clear(partKproduct);
-    	}
+	for (uint64_t i = 0; i < count_matrices + count_S_term; i++)
+	{
+		GxB_kron(partKproduct, NULL, NULL, GrB_LAND, Automat[i], matrices[i], NULL);
+		GrB_eWiseAdd_Matrix_BinaryOp(Kproduct, NULL, NULL, GrB_LOR, Kproduct, partKproduct, NULL);
+		GrB_Matrix_clear(partKproduct);
+	}
 
         GxB_select(Kproduct, NULL, NULL, GxB_NONZERO, Kproduct, NULL, NULL);
-        GxB_print(Kproduct, 3);
 
         // transitive clouser
-		bool transitive_matrix_is_changed = true;
+	bool transitive_matrix_is_changed = true;
 
         info = GrB_Matrix_dup(&degreeKproduct, Kproduct);
         assert(info == GrB_SUCCESS && "2");
@@ -149,64 +161,52 @@ int CFPQ_cpu2(RedisModuleCtx *ctx, GraphContext *gc, bool_automat *grammar, Cfpq
                prev = val_transitive;
             }
         }
-
-        // update graph
 		
-		for (int n = 0; n < count_S_term; n++)
-		{
-			for (uint64_t i = 0; i < sizeAutomat; i++)
-			{
+	for (int n = 0; n < count_S_term; n++)
+	{
+  	    for (uint64_t i = 0; i < sizeAutomat; i++)
+	    {
             	for (uint64_t j = 0; j < sizeAutomat; j++)
             	{
-            	    bool st = false;
-            	    GrB_Matrix_extractElement_BOOL(&st, States[n], i, j);
-            	    if (st)
+            		bool st = false;
+            		GrB_Matrix_extractElement_BOOL(&st, States[n], i, j);
+
+            	if (st)
+            	{
+            	    for (uint64_t k = 0; k < sizeGraph; k++)
             	    {
-            	       GrB_Index I[sizeGraph];
-            	       GrB_Index J[sizeGraph];
-            	       for (uint64_t k = 0; k < sizeGraph; k++)
-            	       {
-            	           I[k] = i * sizeGraph + k;
-            	           J[k] = j * sizeGraph + k;
-            	       }
-            	       GrB_Matrix_extract(block, NULL, NULL, Kproduct, I, sizeGraph, J, sizeGraph, NULL);
+            	        I[k] = i * sizeGraph + k;
+            	        J[k] = j * sizeGraph + k;
+					}
 
-            	       for (uint64_t k = 0; k < sizeGraph; k++)
-            	       {
-            	           for (uint64_t l = 0; l < sizeGraph; l++)
-            	           {
-            	               bool s = false;
-            	               GrB_Matrix_extractElement_BOOL(&s, block, k, l);
-	
-            	               if (s)
-            	               {
-            	                  bool data = false;
-								  int64_t id = grammar->id[count_matrices + n];
-								  uint64_t index = 0;
-								  
-								  while (grammar->id[index] != id)
-								  {
-								      index++;
-								  }
+            	    GrB_Matrix_extract(block, NULL, NULL, Kproduct, I, sizeGraph, J, sizeGraph, NULL);
+			
+					int64_t id = grammar->id[count_matrices + n];
+					int index = 0;
+					while (grammar->id[index] != id)
+					{
+						index++;
+					}
+					GxB_select(block, NULL, NULL, GxB_NONZERO, block, NULL, NULL);
+					GrB_Index val_check = 0;
+					GrB_Matrix_nvals(&val_check, matrices[index]);
+					GrB_Matrix_dup(&prevG, matrices[index]); 
+					GrB_Matrix_apply(matrices[index], NULL, NULL, GxB_ONE_BOOL, block, NULL);
+					GrB_eWiseAdd_Matrix_BinaryOp(matrices[index], NULL, NULL, GrB_LOR, prevG, matrices[index], NULL);
+					GrB_Index nval_check = 0;
+					GxB_select(matrices[index], NULL, NULL, GxB_NONZERO, matrices[index], NULL, NULL);
 
-            	                  GrB_Matrix_extractElement_BOOL(&data, matrices[index], k, l);
+					GrB_Matrix_nvals(&nval_check, matrices[index]);
 
-            	                  if (!data)
-            	                  {
-            	                     nvals+=2;
-									 matrices_is_changed = true;
-            	                     GrB_Matrix_setElement_BOOL(matrices[index], true, k, l);
-            	                     GrB_Matrix_setElement_BOOL(matrices[index], true, l, k);
-            	                  }
-            	               }
-                       		}
-                   		}
-                	}
-            	}
+					if (val_check != nval_check)
+					    matrices_is_changed = true;
+					GrB_Matrix_nvals(&nvals, matrices[index]);
+			
+                }
+            }
 
-        	} 
-		}
-	
+		} 
+	}
         GrB_Matrix_clear(degreeKproduct);
         GrB_Matrix_clear(Kproduct);
     }
@@ -219,6 +219,7 @@ int CFPQ_cpu2(RedisModuleCtx *ctx, GraphContext *gc, bool_automat *grammar, Cfpq
     GrB_Matrix_free(&Kproduct);
     GrB_Matrix_free(&degreeKproduct);
     GrB_Matrix_free(&partKproduct);
+    GrB_Matrix_free(&prevG);
 
 	for (uint64_t i = 0; i < count_automat; i++)
 		GrB_Matrix_free(&Automat[i]);
@@ -226,7 +227,7 @@ int CFPQ_cpu2(RedisModuleCtx *ctx, GraphContext *gc, bool_automat *grammar, Cfpq
     for (uint64_t i = 0; i < count_matrices + count_S_term; i++)
          GrB_Matrix_free(&matrices[i]);
 
-	MemInfo mem_delta;
+    MemInfo mem_delta;
     mem_usage_tok(&mem_delta, mem_start);
     response->vms_dif = mem_delta.vms;
     response->rss_dif = mem_delta.rss;
@@ -234,9 +235,3 @@ int CFPQ_cpu2(RedisModuleCtx *ctx, GraphContext *gc, bool_automat *grammar, Cfpq
 
     return REDISMODULE_OK;
 }
-
-
-
-
-			
-	
